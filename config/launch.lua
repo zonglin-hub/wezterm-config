9a82e5302d833c52e28f0303deee11879f9d0fca
liuzonglin <lzlysfxx@163.com>
2024-03-25 00:39:23 +0800

    首次提交

---
+++ config/launch.lua
@@ -0,0 +1,41 @@
+local platform = require('utils.platform')()
+
+local options = {
+   default_prog = {},
+   launch_menu = {},
+}
+
+if platform.is_win then
+   options.default_prog = { 'powershell' }
+   options.launch_menu = {
+      { label = 'PowerShell Core', args = { 'pwsh' } },
+      { label = 'PowerShell Desktop', args = { 'powershell' } },
+      { label = 'Command Prompt', args = { 'cmd' } },
+      { label = 'Nushell', args = { 'nu' } },
+      {
+         label = 'Git Bash',
+         args = { 'C:\\Users\\kevin\\scoop\\apps\\git\\current\\bin\\bash.exe' },
+      },
+      {
+         label = 'Ubuntu 22.04.2 LTS (GNU/Linux 5.15.0-101-generic x86_64)',
+         args = { 'ssh', 'liu@172.20.119.226', '-p', '22' },
+      },
+   }
+elseif platform.is_mac then
+   options.default_prog = { '/opt/homebrew/bin/fish' }
+   options.launch_menu = {
+      { label = 'Bash', args = { 'bash' } },
+      { label = 'Fish', args = { '/opt/homebrew/bin/fish' } },
+      { label = 'Nushell', args = { '/opt/homebrew/bin/nu' } },
+      { label = 'Zsh', args = { 'zsh' } },
+   }
+elseif platform.is_linux then
+   options.default_prog = { 'fish' }
+   options.launch_menu = {
+      { label = 'Bash', args = { 'bash' } },
+      { label = 'Fish', args = { 'fish' } },
+      { label = 'Zsh', args = { 'zsh' } },
+   }
+end
+
+return options
