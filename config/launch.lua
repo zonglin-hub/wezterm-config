499482a0b3826b20b1cc4dde8283e8eefb193fc7
liuzonglin <lzlysfxx@163.com>
2024-04-09 23:57:53 +0800

    移除 Git Bash; 简写 Ubuntu: 文件格式化

--- config/launch.lua
+++ config/launch.lua
@@ -1,4 +1,4 @@
--- 不同系统启动菜单 -- 
+-- 不同系统启动菜单 --
 
 local platform = require('utils.platform')()
 
@@ -10,35 +10,29 @@ local options = {
 if platform.is_win then
    options.default_prog = { 'nu' }
    options.launch_menu = {
+
       -- 卸载 PowerShell Core，禁用
       -- { label = 'PowerShell Core', args = { 'pwsh' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Nushell', args = { 'nu' } },
-      {
-         label = 'Git Bash',
-         args = { 'D:\\Program Files\\Git\\git-bash.exe' },
-      },
-      {
-         label = 'Ubuntu 22.04.2 LTS (GNU/Linux 5.15.0-101-generic x86_64)',
-         args = { 'ssh', 'zonglin@172.20.115.202', '-p', '22' },
-      },
+      { label = 'Command Prompt',     args = { 'cmd' } },
+      { label = 'Nushell',            args = { 'nu' } },
+      { label = 'Ubuntu',             args = { 'ssh', 'zonglin@172.20.115.202', '-p', '22' } },
    }
 elseif platform.is_mac then
    options.default_prog = { 'bash' }
    options.launch_menu = {
-      { label = 'Bash', args = { 'bash' } },
-      { label = 'Fish', args = { 'fish' } },
+      { label = 'Bash',    args = { 'bash' } },
+      { label = 'Fish',    args = { 'fish' } },
       { label = 'Nushell', args = { 'nu' } },
-      { label = 'Zsh', args = { 'zsh' } },
+      { label = 'Zsh',     args = { 'zsh' } },
    }
 elseif platform.is_linux then
    options.default_prog = { 'bash' }
    options.launch_menu = {
-      { label = 'Bash', args = { 'bash' } },
-      { label = 'Fish', args = { 'fish' } },
+      { label = 'Bash',    args = { 'bash' } },
+      { label = 'Fish',    args = { 'fish' } },
       { label = 'Nushell', args = { 'nu' } },
-      { label = 'Zsh', args = { 'zsh' } },
+      { label = 'Zsh',     args = { 'zsh' } },
    }
 end
 
